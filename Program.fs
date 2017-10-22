c203ba2deb2e908390e68d1c74437ad42b8837fb
Tomas Jansson <tomas.jansson@unacast.com>
2017-10-22 22:12:41 +0200

    initial commit

---
+++ Program.fs
@@ -0,0 +1,30 @@
+﻿open System
+open Proto
+open System.Threading.Tasks
+open Proto.FSharp
+
+type Message = {Text: string}
+type Message2 = 
+    | Text of string
+
+[<EntryPoint>]
+let main argv =
+    let handleMessage msg = 
+        match msg with
+        | Text t -> printfn "This could work: %s" t
+
+    let pid = 
+        simpleProducer handleMessage
+        |> fromProducer
+        |> spawn
+
+    [1 .. 1000]
+    |> List.map (fun i -> Text (sprintf "Tomas %d" i))
+    |> List.iter (tell pid)
+
+    //Proto.createActor handleMessage // Actor.FromProducer(fun () -> (new MyActor() :> IActor))
+    // let pid = Actor.Spawn(props)
+    // pid.Tell({Text = "Tomas"})
+    // pid.Tell(Text "Tomas")
+    System.Console.ReadLine() |> ignore
+    0
---
+++ Proto.FSharp.fs
@@ -0,0 +1,82 @@
+module Proto.FSharp
+
+open Proto
+
+module System = 
+    open System
+
+    let toFunc<'a> f = Func<'a>(f)
+    let toFunc2<'a, 'b> f = Func<'a, 'b>(f)
+
+[<AutoOpen>]
+module Props = 
+    open System
+
+    let newProps() = Props()
+
+    let withProducer producer (props: Props) = 
+        props.WithProducer(producer)
+
+    let withDispatcher dispatcher (props: Props) = 
+        props.WithDispatcher(dispatcher)
+
+    let withMailbox mailbox (props: Props) = 
+        props.WithMailbox(mailbox)
+
+    let withChildSupervisorStrategy supervisorStrategy (props: Props) =
+        props.WithChildSupervisorStrategy(supervisorStrategy)
+
+    let withReceiveMiddleware (middleware: Receive -> Receive) (props: Props) =
+        props.WithReceiveMiddleware([|toFunc2(middleware)|])
+
+    let withReceiveMiddlewares (middlewares: (Receive -> Receive) list) (props: Props) =
+        middlewares 
+        |> List.map toFunc2
+        |> Array.ofList
+        |> (fun arr -> props.WithReceiveMiddleware(arr))
+
+    let withSenderMiddleware (middleware: Sender -> Sender) (props: Props) =
+        props.WithSenderMiddleware([|toFunc2(middleware)|])
+
+    let withSenderMiddlewares (middlewares: (Sender -> Sender) list) (props: Props) =
+        middlewares 
+        |> List.map toFunc2
+        |> Array.ofList
+        |> (fun arr -> props.WithSenderMiddleware(arr))
+
+    let withSpawner spawner (props: Props) = 
+        props.WithSpawner(spawner)
+
+[<AutoOpen>]
+module Actor =
+    open System
+
+    let spawn (props: Props) = Actor.Spawn(props)
+
+    let spawnPrefix prefix (props: Props) = Actor.SpawnPrefix(props, prefix)
+
+    let spawnNamed name (props: Props) = Actor.SpawnNamed(props, name)
+
+    let simpleProducer<'T> f =
+        fun () ->
+            { new IActor with 
+                member this.ReceiveAsync(context: IContext) = 
+                    let msg = context.Message
+                    match msg with
+                    | :? 'T as x -> f x
+                    | :? Proto.Started
+                    | :? Proto.Restarting 
+                    | :? Proto.Stopping -> 
+                        printfn "Internal message: %A" msg
+                    | _ -> 
+                        raise (exn (sprintf "Invalid message: %A" msg)) 
+                    Actor.Done }
+
+    let fromProducer producer = Actor.FromProducer(toFunc(producer))
+
+    let fromFunc receive = Actor.FromFunc(receive)
+
+[<AutoOpen>]
+module Pid = 
+    let tell (pid: PID) msg = 
+        pid.Tell(msg) 
