123484c68daf8fb204d26256b4159f7f40cca0dd
Tomas Jansson <tomas.jansson@unacast.com>
2017-11-26 13:29:01 +0100

    some more structures and helpers

--- src/Program.fs
+++ src/Program.fs
@@ -1,244 +1,75 @@
 ﻿open System
-open Proto
-open System.Threading.Tasks
 open Proto.FSharp
 
-type Message = {Text: string}
-type Message2 = 
-    | Text of string
+type Person = 
+    {
+        Age: int
+        Name: string
+    }
 
-let test1() = 
-    let handleMessage (msg: obj) = 
-        match msg with
-        | :? Message2 as m ->
-            match m with
-            | Text t -> printfn "This could work: %s" t
+type SetName = { Name : string }
+type SetAge = { Age : int }
 
-    let pid = 
-        simpleProducer handleMessage
-        |> fromProducer
-        |> spawn
+type Messages =
+    | SetName of SetName
+    | SetAge of SetAge
+    | Unknown
 
-    [1 .. 1000]
-    |> List.map (fun i -> Text (sprintf "Tomas %d" i))
-    |> List.iter (tell pid)
-
-    //Proto.createActor handleMessage // Actor.FromProducer(fun () -> (new MyActor() :> IActor))
-    // let pid = Actor.Spawn(props)
-    // pid.Tell({Text = "Tomas"})
-    // pid.Tell(Text "Tomas")
-
-module ProtoFSharp = 
-    type IO<'T> =
-        | Input
-
-    type Actor<'Message> = 
-        abstract Receive : unit -> IO<'Message>
-
-
-    type Cont<'In, 'Out> = 
-        | Func of ('In -> Cont<'In, 'Out>)
-        | Return of 'Out
-
-    /// The builder for actor computation expression.
-    type ProtoBuilder() = 
-    
-        /// Binds the next message.
-        member __.Bind(m : IO<'In>, f : 'In -> _) = Func(fun m -> f m)
-    
-        /// Binds the result of another actor computation expression.
-        member this.Bind(x : Cont<'In, 'Out1>, f : 'Out1 -> Cont<'In, 'Out2>) : Cont<'In, 'Out2> = 
-            match x with
-            | Func fx -> Func(fun m -> this.Bind(fx m, f))
-            | Return v -> f v
-    
-        member __.ReturnFrom(x) = x
-        member __.Return x = Return x
-        member __.Zero() = Return()
-    
-        member this.TryWith(f : unit -> Cont<'In, 'Out>, c : exn -> Cont<'In, 'Out>) : Cont<'In, 'Out> = 
-            try 
-                true, f()
-            with ex -> false, c ex
-            |> function 
-            | true, Func fn -> Func(fun m -> this.TryWith((fun () -> fn m), c))
-            | _, v -> v
-    
-        member this.TryFinally(f : unit -> Cont<'In, 'Out>, fnl : unit -> unit) : Cont<'In, 'Out> = 
-            try 
-                match f() with
-                | Func fn -> Func(fun m -> this.TryFinally((fun () -> fn m), fnl))
-                | r -> 
-                    fnl()
-                    r
-            with ex -> 
-                fnl()
-                reraise()
-    
-        member this.Using(d : #IDisposable, f : _ -> Cont<'In, 'Out>) : Cont<'In, 'Out> = 
-            this.TryFinally((fun () -> f d), 
-                            fun () -> 
-                                if d <> null then d.Dispose())
-    
-        member this.While(condition : unit -> bool, f : unit -> Cont<'In, unit>) : Cont<'In, unit> = 
-            if condition() then 
-                match f() with
-                | Func fn -> 
-                    Func(fun m -> 
-                        fn m |> ignore
-                        this.While(condition, f))
-                | v -> this.While(condition, f)
-            else Return()
-    
-        member __.For(source : 'Iter seq, f : 'Iter -> Cont<'In, unit>) : Cont<'In, unit> = 
-            use e = source.GetEnumerator()
-        
-            let rec loop() = 
-                if e.MoveNext() then 
-                    match f e.Current with
-                    | Func fn -> 
-                        Func(fun m -> 
-                            fn m |> ignore
-                            loop())
-                    | r -> loop()
-                else Return()
-            loop()
-    
-        member __.Delay(f : unit -> Cont<_, _>) = f
-        member __.Run(f : unit -> Cont<_, _>) = f()
-        member __.Run(f : Cont<_, _>) = f
-    
-        member this.Combine(f : unit -> Cont<'In, _>, g : unit -> Cont<'In, 'Out>) : Cont<'In, 'Out> = 
-            match f() with
-            | Func fx -> Func(fun m -> this.Combine((fun () -> fx m), g))
-            | Return _ -> g()
-    
-        member this.Combine(f : Cont<'In, _>, g : unit -> Cont<'In, 'Out>) : Cont<'In, 'Out> = 
-            match f with
-            | Func fx -> Func(fun m -> this.Combine(fx m, g))
-            | Return _ -> g()
-    
-        member this.Combine(f : unit -> Cont<'In, _>, g : Cont<'In, 'Out>) : Cont<'In, 'Out> = 
-            match f() with
-            | Func fx -> Func(fun m -> this.Combine((fun () -> fx m), g))
-            | Return _ -> g
-    
-        member this.Combine(f : Cont<'In, _>, g : Cont<'In, 'Out>) : Cont<'In, 'Out> = 
-            match f with
-            | Func fx -> Func(fun m -> this.Combine(fx m, g))
-            | Return _ -> g
-
-    let proto = ProtoBuilder()
-
-open ProtoFSharp
-open System.Security.Cryptography
-open System.Runtime.Serialization
-
-module Spawn =
-    open Proto
-    let spawn (createActor) =
-        let inline startAsPlainTask (work : Async<unit>) = Task.Factory.StartNew(fun () -> work |> Async.RunSynchronously)
-
-
-        let producer () =
-    // public interface IActor
-    // {
-    //     Task ReceiveAsync(IContext context);
-    // }
-            let mutable state = createActor { new Actor<'T1> with member this.Receive() = Input }
-            { new IActor with
-                member this.ReceiveAsync(context: IContext) =
-                    async {
-                        match state with
-                        | Func f ->
-                            match context.Message with
-                            | :? 'T1 as msg -> 
-                                state <- f msg
-                            | _ -> ()
-                    } |> startAsPlainTask
-            }
-
-        let producerFunc = System.Func<_>(producer)
-        let props = Actor.FromProducer(producerFunc)
-        Actor.spawn props
-
-type Messages = 
-    | Hello
-    | World of string
-
-type Msg1 = { Name : string }
-type Msg2 = { Age : int }
-
-let (|Msg1|_|) (msg: obj) =
+let (|IsMessage|_|) (msg: obj) =
     match msg with
-    | :? Msg1 as m -> Some(m)
-    | _ -> None
-let (|Msg2|_|) (msg: obj) =
-    match msg with
-    | :? Msg2 as m -> Some(m)
+    | :? SetAge as m -> Some(SetAge m)
+    | :? SetName as m -> Some(SetName m)
     | _ -> None
 
 let test2() =
-    let proton = (fun (mailbox: Actor<string>) ->
-            let rec loop state = proto {
-                let! message = mailbox.Receive()
-                printfn "Received: %A" message
-                // handle an incoming message
-                let state' = state + "." + message
-                printfn "State: %A" state'
-                return! loop state'
-            }
-            loop "")
+    let handler s m = 
+        printfn "Received: %A" m
+        let s' = s + "." + m
+        printfn "State: %A" s'
+        s'
 
-    // use system = System.create "my-system" (Configuration.load())
-    let pid = Spawn.spawn proton // spawn system "my-actor" proton
+    let pid =
+        Actor.withState handler ""
+        |> Actor.initProps
+        |> Actor.spawn
     pid <! "proto"
     pid <! "actor"
 
-    let proton2 (mailbox:Actor<Messages>) =
-        let rec loop state = proto {
-            let! message = mailbox.Receive()
-            printfn "Received: %A" message
-            let state' = message :: state
-            printfn "State: %A" state'
-            return! loop state'
-        }
-        loop []
-
-    let pid2 = Spawn.spawn proton2
-    pid2 <! Hello
-    pid2 <! World "Tellus"
-
-
-    let pid3 = Spawn.spawn 
-                (fun (mailbox: Actor<obj>) -> 
-                    let rec loop() = proto { 
-                        let! message = mailbox.Receive()
-                        printfn "%A" message
-                        return! loop() } 
-                    in loop())
+    let pid3 = Actor.create (printfn "Msg: %A") |> Actor.initProps |> Actor.spawn
     [ 1 .. 100 ] |> List.iter (fun i -> pid3 <! i)
 
-    let pid4 = Spawn.spawn 
-                (fun (mailbox: Actor<obj>) -> 
-                    let rec loop() = proto { 
-                        let! message = mailbox.Receive()
-                        match message with
-                        | Msg1 m -> printfn "Message1: %A" m
-                        | Msg2 m -> printfn "Message2: %A" m
-                        | _ -> printfn "Unknown message: %A" message
-                        return! loop() } 
-                    in loop())
+    let mapMsg (msg: obj) =
+        match msg with
+        | IsMessage m -> m
+        | _ -> Unknown
+
+    let handler4 (msg: Messages) state = 
+        let state' : Person = 
+            match msg with
+            | SetName m -> 
+                printfn "SetName: %A" m
+                { state with Name = m.Name }
+            | SetAge m -> 
+                printfn "SetAge: %A" m
+                { state with Age = m.Age }
+            | Unknown -> 
+                printfn "unknown %A" msg
+                state
+        printfn "Current state: %A" state'
+        state'
+
+    let pid4 =
+        Actor.withState (mapMsg >> handler4) { Name = "John"; Age = 0}
+        |> Actor.initProps 
+        |> Actor.spawn
+
     pid4 <! { Name = "tomas" }
     pid4 <! { Age = 35 }
     pid4 <! "this isn't handled"
     ()
 
-
 [<EntryPoint>]
 let main argv =
     test2()
-
-
     System.Console.ReadLine() |> ignore
     0
--- src/Proto.FSharp.fs
+++ src/Proto.FSharp.fs
@@ -1,12 +1,172 @@
 module Proto.FSharp
 
 open Proto
+open System.Threading.Tasks
+open System
+
+let inline startAsPlainTask (work : Async<unit>) = Task.Factory.StartNew(fun () -> work |> Async.RunSynchronously)
 
 module System = 
-    open System
-
     let toFunc<'a> f = Func<'a>(f)
     let toFunc2<'a, 'b> f = Func<'a, 'b>(f)
+
+module Actor =
+    let spawn (props: Props) = Actor.Spawn(props)
+
+    let spawnPrefix prefix (props: Props) = Actor.SpawnPrefix(props, prefix)
+
+    let spawnNamed name (props: Props) = Actor.SpawnNamed(props, name)
+
+    type IO<'T> =
+        | Input
+
+    type Actor<'Message> = 
+        abstract Receive : unit -> IO<'Message>
+        abstract CurrentContext: unit -> IContext
+
+
+    type Cont<'In, 'Out> = 
+        | Func of ('In -> Cont<'In, 'Out>)
+        | Return of 'Out
+
+    /// The builder for actor computation expression.
+    type ProtoBuilder() = 
+    
+        /// Binds the next message.
+        member __.Bind(m : IO<'In>, f : 'In -> _) = Func(f)
+    
+        /// Binds the result of another actor computation expression.
+        member this.Bind(x : Cont<'In, 'Out1>, f : 'Out1 -> Cont<'In, 'Out2>) : Cont<'In, 'Out2> = 
+            match x with
+            | Func fx -> Func(fun m -> this.Bind(fx m, f))
+            | Return v -> f v
+
+        member __.ReturnFrom(x) = x
+        member __.Return x = Return x
+        member __.Zero() = Return()
+    
+        member this.TryWith(f : unit -> Cont<'In, 'Out>, c : exn -> Cont<'In, 'Out>) : Cont<'In, 'Out> = 
+            try 
+                true, f()
+            with ex -> false, c ex
+            |> function 
+            | true, Func fn -> Func(fun m -> this.TryWith((fun () -> fn m), c))
+            | _, v -> v
+    
+        member this.TryFinally(f : unit -> Cont<'In, 'Out>, fnl : unit -> unit) : Cont<'In, 'Out> = 
+            try 
+                match f() with
+                | Func fn -> Func(fun m -> this.TryFinally((fun () -> fn m), fnl))
+                | r -> 
+                    fnl()
+                    r
+            with ex -> 
+                fnl()
+                reraise()
+    
+        member this.Using(d : #IDisposable, f : _ -> Cont<'In, 'Out>) : Cont<'In, 'Out> = 
+            this.TryFinally((fun () -> f d), 
+                            fun () -> 
+                                if d <> null then d.Dispose())
+    
+        member this.While(condition : unit -> bool, f : unit -> Cont<'In, unit>) : Cont<'In, unit> = 
+            if condition() then 
+                match f() with
+                | Func fn -> 
+                    Func(fun m -> 
+                        fn m |> ignore
+                        this.While(condition, f))
+                | v -> this.While(condition, f)
+            else Return()
+    
+        member __.For(source : 'Iter seq, f : 'Iter -> Cont<'In, unit>) : Cont<'In, unit> = 
+            use e = source.GetEnumerator()
+        
+            let rec loop() = 
+                if e.MoveNext() then 
+                    match f e.Current with
+                    | Func fn -> 
+                        Func(fun m -> 
+                            fn m |> ignore
+                            loop())
+                    | r -> loop()
+                else Return()
+            loop()
+    
+        member __.Delay(f : unit -> Cont<_, _>) = f
+        member __.Run(f : unit -> Cont<_, _>) = f()
+        member __.Run(f : Cont<_, _>) = f
+    
+        member this.Combine(f : unit -> Cont<'In, _>, g : unit -> Cont<'In, 'Out>) : Cont<'In, 'Out> = 
+            match f() with
+            | Func fx -> Func(fun m -> this.Combine((fun () -> fx m), g))
+            | Return _ -> g()
+    
+        member this.Combine(f : Cont<'In, _>, g : unit -> Cont<'In, 'Out>) : Cont<'In, 'Out> = 
+            match f with
+            | Func fx -> Func(fun m -> this.Combine(fx m, g))
+            | Return _ -> g()
+    
+        member this.Combine(f : unit -> Cont<'In, _>, g : Cont<'In, 'Out>) : Cont<'In, 'Out> = 
+            match f() with
+            | Func fx -> Func(fun m -> this.Combine((fun () -> fx m), g))
+            | Return _ -> g
+    
+        member this.Combine(f : Cont<'In, _>, g : Cont<'In, 'Out>) : Cont<'In, 'Out> = 
+            match f with
+            | Func fx -> Func(fun m -> this.Combine(fx m, g))
+            | Return _ -> g
+
+    type FSharpActor<'Message, 'ReturnType>(createActor: Actor<'Message> -> Cont<'Message, 'Returned>) as this = 
+        let actor = 
+            { 
+                new Actor<'T1> 
+                    with 
+                        member __.Receive() = Input
+                        member __.CurrentContext() = this.CurrentContext()}
+        let mutable state = createActor actor
+        let mutable ctx: IContext = null
+        member __.CurrentContext() = 
+            ctx
+
+        interface IActor with
+            member this.ReceiveAsync(context: IContext) =
+                async {
+                    match state with
+                    | Func f ->
+                        ctx <- context
+                        match context.Message with
+                        | :? 'T1 as msg -> 
+                            state <- f msg
+                        | _ -> ()
+                    | Return x -> x
+                } |> startAsPlainTask
+
+    let initProps (createActor: Actor<'Message> -> Cont<'Message, 'Returned>) =
+        let producer () = new FSharpActor<'Message, 'Returned>(createActor) :> IActor
+        let producerFunc = System.Func<_>(producer)
+        Actor.FromProducer(producerFunc)
+
+    let proto = ProtoBuilder()
+
+    let withState2 (handler: Actor<'Message> -> 'Message -> 'State -> 'State) (initialState: 'State) (mailbox : Actor<'Message>) =
+        let rec loop state = 
+            proto {
+                let! msg = mailbox.Receive()
+                let state' = state |> handler mailbox msg
+                return! loop state'
+            }
+        loop initialState
+
+    let withState (handler: 'Message -> 'State -> 'State) (initialState: 'State) =
+        withState2 (fun _ m s -> handler m s) initialState
+
+    let create (handler: 'Message -> unit) =
+        withState2 (fun _ m _ -> handler m) ()
+
+    let create2 (handler: Actor<'Message> -> 'Message -> unit) =
+        withState2 (fun mb m _ -> handler mb m) ()
+
 
 [<AutoOpen>]
 module Props = 
@@ -46,35 +206,6 @@ module Props =
 
     let withSpawner spawner (props: Props) = 
         props.WithSpawner(spawner)
-
-[<AutoOpen>]
-module Actor =
-    open System
-
-    let spawn (props: Props) = Actor.Spawn(props)
-
-    let spawnPrefix prefix (props: Props) = Actor.SpawnPrefix(props, prefix)
-
-    let spawnNamed name (props: Props) = Actor.SpawnNamed(props, name)
-
-    let simpleProducer (f: obj -> unit) =
-        fun () ->
-            { new IActor with 
-                member this.ReceiveAsync(context: IContext) = 
-                    let msg = context.Message
-                    match msg with
-                    | :? Proto.Started
-                    | :? Proto.Restarting 
-                    | :? Proto.Stopping -> 
-                        printfn "Internal message: %A" msg
-                    | x -> f x
-                    Actor.Done }
-
-    let fromProducer producer = Actor.FromProducer(toFunc(producer))
-
-    let fromFunc receive =
-        Receive(receive)
-        |> Actor.FromFunc
 
 [<AutoOpen>]
 module Pid = 
